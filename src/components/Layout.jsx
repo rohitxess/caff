d1088f0a9ddcb35a0294c695558df657dee23de7
rohitxess <xessro@gmail.com>
2025-06-04 13:08:58 +0530

    main layout changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -13,8 +13,8 @@ export default function Layout(props) {
     const header = (
         <header>
             <div>
-                <h1 className="text-gradient">CAFFIEND</h1>
-                <p>For Coffee Insatiates</p>
+                <h1 className="text-gradient">Caff&Choc</h1>
+                <p>For Coffee Meetings ☕🍫</p>
             </div>
             {globalUser ? (
                 <button onClick={logout}>
@@ -31,7 +31,7 @@ export default function Layout(props) {
 
     const footer = (
         <footer>
-            <p><span className="text-gradient">Caffiend</span> was made by <a target="_blank" href="https://www.smoljames.com">Smoljames</a> <br />using the <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.<br />Check out the project on <a target="_black" href="https://www.github.com/jamezmca/reactjs-full-course">GitHub</a>!</p>
+            <p><span className="text-gradient">Made with</span><a target="_blank" href="https://github.com/rohitxess">🩵</a> <br />using the <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.<br />Check out the project on <a target="_black" href="https://www.github.com/jamezmca/reactjs-full-course">GitHub</a>!</p>
         </footer>
     )
 
